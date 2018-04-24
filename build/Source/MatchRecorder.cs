756874dd294233765d980e61d29969432555f596
Jvs <jvstheluacoder@gmail.com>
2018-04-24 13:23:28 +0200

    Fixed obs socket reference by adding them manually to the project

--- build/Source/MatchRecorder.cs
+++ build/Source/MatchRecorder.cs
@@ -3,9 +3,28 @@ using System.Linq;
 using System.Reflection;
 using Harmony;
 using DuckGame;
+using OBSWebsocketDotNet;
 
 namespace MatchRecorder
 {
+	public class MatchRecorderHandler
+	{
+		private OBSWebsocket handler;
+
+		public MatchRecorderHandler()
+		{
+			handler = new OBSWebsocket();
+			
+		}
+	}
 
 
+
+
+
+
+	public class MatchRecorder_Overrides
+	{
+
+	}
 }
--- build/Source/Mod.cs
+++ build/Source/Mod.cs
@@ -5,13 +5,18 @@ namespace MatchRecorder
 {
 	public class Mod : DuckGame.Mod
 	{
+		public static MatchRecorderHandler matchRecorderSingleton = null;
+
 		protected override void OnPreInitialize()
 		{
 #if false
 			System.Diagnostics.Debugger.Launch();
 #endif
+			matchRecorderSingleton = new MatchRecorderHandler();
 
+			//HUD.AddInputChangeDisplay
 			HarmonyInstance.Create( "MatchRecorder" ).PatchAll( Assembly.GetExecutingAssembly() );
+
 		}
 	}
 }
