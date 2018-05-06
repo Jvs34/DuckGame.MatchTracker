b9205c5211b197d28a7c333b496113d0f0130dd1
Jvs <jvstheluacoder@gmail.com>
2018-05-06 17:12:44 +0200

    Apparently bootstrap already has a card element, whoops

--- MatchViewer/Program.cs
+++ MatchViewer/Program.cs
@@ -5,16 +5,16 @@ using System;
 
 namespace MatchViewer
 {
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            var serviceProvider = new BrowserServiceProvider(services =>
-            {
-                // Add any custom services here
-            });
+	public class Program
+	{
+		static void Main( string [] args )
+		{
+			var serviceProvider = new BrowserServiceProvider( services =>
+			 {
+				// Add any custom services here
+			} );
 
-            new BrowserRenderer(serviceProvider).AddComponent<App>("app");
-        }
-    }
+			new BrowserRenderer( serviceProvider ).AddComponent<App>( "app" );
+		}
+	}
 }
