1eaaf07c711c49b81b3d69ebe2ab8fe2a6b62a28
Jvs <jvstheluacoder@gmail.com>
2018-05-07 16:20:28 +0200

    Started work on the match database

---
+++ MatchViewer/MatchDatabase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using MatchTracker;
+
+namespace MatchViewer
+{
+	public interface IMatchDatabase
+	{
+
+	}
+
+    public class MatchDatabase : IMatchDatabase
+    {
+		private HttpClient client;
+		private String baseRepositoryURL;
+		private SharedSettings sharedSettings;
+
+		public MatchDatabase( HttpClient givenClient )
+		{
+			givenClient = client;
+			baseRepositoryURL = "https://raw.githubusercontent.com/Jvs34/DuckGame.MatchDB/master/";
+		}
+    }
+}
