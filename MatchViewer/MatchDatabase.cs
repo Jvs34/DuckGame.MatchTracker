7fdc20ece99419535dd6cb325da8bb0ac66c89cf
Jvs <jvstheluacoder@gmail.com>
2018-05-07 21:18:51 +0200

    Added website project back into the solution

--- MatchViewer/MatchDatabase.cs
+++ MatchViewer/MatchDatabase.cs
@@ -4,24 +4,45 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using MatchTracker;
+using Flurl;
 
 namespace MatchViewer
 {
 	public interface IMatchDatabase
 	{
+		Task<GlobalData> GetGlobalData();
+
+		Task<MatchData> GetMatchData( String matchName );
+
+		Task<RoundData> GetRoundData( String roundName );
 
 	}
 
     public class MatchDatabase : IMatchDatabase
     {
 		private HttpClient client;
-		private String baseRepositoryURL;
+		private Uri baseRepositoryURL;
 		private SharedSettings sharedSettings;
 
 		public MatchDatabase( HttpClient givenClient )
 		{
 			givenClient = client;
-			baseRepositoryURL = "https://raw.githubusercontent.com/Jvs34/DuckGame.MatchDB/master/";
+			baseRepositoryURL = new Uri("https://raw.githubusercontent.com/Jvs34/DuckGame.MatchDB/master/");
 		}
-    }
+
+		public Task<GlobalData> GetGlobalData()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<MatchData> GetMatchData( string matchName )
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<RoundData> GetRoundData( string roundName )
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
