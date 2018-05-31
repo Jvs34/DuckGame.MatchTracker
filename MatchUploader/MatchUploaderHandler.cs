206b05b506af79f6617d373b81ca4fa57e0b1fe2
Jvs <jvstheluacoder@gmail.com>
2018-05-31 18:08:02 +0200

    Removed deprecated resumable stuff

--- MatchUploader/MatchUploaderHandler.cs
+++ MatchUploader/MatchUploaderHandler.cs
@@ -529,8 +529,6 @@ namespace MatchUploader
 				currentVideo = null;
 
 			}
-
-
 		}
 
 
--- MatchUploader/UploaderSettings.cs
+++ MatchUploader/UploaderSettings.cs
@@ -9,10 +9,6 @@ namespace MatchUploader
 {
 	public class UploaderSettings
 	{
-		//deprecated
-		public String uploadToResume = null; //this is the round name itself, we can get the path to the video file later
-		public Uri uploadToResumeURI = null;
-		//
 		public Dictionary<String , Uri> pendingUploads = new Dictionary<String , Uri>();
 
 		public float uploadSpeed = 0; //in kylobytes per seconds, 0 means no throttling
