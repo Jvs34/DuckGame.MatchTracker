481a427570748482e60e01124b66c4b8c2aa38a4
Jvs <jvstheluacoder@gmail.com>
2018-07-01 23:52:14 +0200

    Added shared directory.build.props to avoid adding same info to all packages constantly

---
+++ MatchViewerOoui/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Ooui;
+using Xamarin.Forms;
+
+namespace MatchViewerOoui
+{
+	static class Program
+	{
+		static void Main( string [] args )
+		{
+
+			Forms.Init();
+
+			// Create the UI
+			var page = new ContentPage();
+			var stack = new StackLayout();
+			var button = new Xamarin.Forms.Button
+			{
+				Text = "Click me!"
+			};
+			stack.Children.Add( button );
+			page.Content = stack;
+
+			// Add some logic to it
+			var count = 0;
+			button.Clicked += ( s , e ) =>
+			{
+				count++;
+				button.Text = $"Clicked {count} times";
+			};
+
+			// Publish a root element to be displayed
+			UI.Port = 9090;
+			UI.Host = "localhost";
+			UI.Publish( "/" , page.GetOouiElement() );
+			Console.ReadLine();
+
+		}
+	}
+}
