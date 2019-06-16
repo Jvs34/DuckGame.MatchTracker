b278837f9314b52fef46c50caae016f708f4ac8e
Jvs <jvstheluacoder@gmail.com>
2019-06-16 16:13:30 +0200

    Simplify github pushing a little bit

--- MatchOctoKitDatabase/OctoKitGameDatabase.cs
+++ MatchOctoKitDatabase/OctoKitGameDatabase.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -50,29 +51,35 @@ namespace MatchTracker
 
 			SetCachedItem( data , DateTime.UtcNow.AddSeconds( 150 ) );
 
+			IReadOnlyList<RepositoryContent> fileContents = new List<RepositoryContent>();
+
 			try
 			{
 				//I don't know why this would throw an exception instead of just giving an empty list but WHATEVER MAN
-				IReadOnlyList<RepositoryContent> fileContents = await OctoKitClient.Repository.Content.GetAllContents( SharedSettings.RepositoryUser , SharedSettings.RepositoryName , url );
-				if( fileContents?.Count > 0 )
-				{
-					await OctoKitClient.Repository.Content.UpdateFile(
-						SharedSettings.RepositoryUser ,
-						SharedSettings.RepositoryName ,
-						url ,
-						new UpdateFileRequest( $"Updated {typeof( T )} : {data.DatabaseIndex}" , newFileContent.ToString() , fileContents [0].Sha , true )
-					);
-				}
+				fileContents = await OctoKitClient.Repository.Content.GetAllContents( SharedSettings.RepositoryUser , SharedSettings.RepositoryName , url );
 			}
 			catch( Exception )
 			{
+				Console.WriteLine( $"Could not find {url}, creating." );
+			}
+
+			RepositoryContent fileInfo = fileContents.FirstOrDefault();
+
+			UpdateFileRequest contentRequest = new UpdateFileRequest(
+				$"{( fileInfo != null ? "Updated" : "Created" )} {typeof( T )} : {data.DatabaseIndex}" ,
+				newFileContent.ToString() ,
+				fileInfo != null ? fileInfo.Sha : string.Empty ,
+				true
+			);
+
+			if( fileInfo != null )
+			{
+				await OctoKitClient.Repository.Content.UpdateFile( SharedSettings.RepositoryUser , SharedSettings.RepositoryName , url , contentRequest );
+			}
+			else
+			{
 				//the file was not found, this is totally fine, we'll create it now
-				await OctoKitClient.Repository.Content.CreateFile(
-					SharedSettings.RepositoryUser ,
-					SharedSettings.RepositoryName ,
-					url ,
-					new CreateFileRequest( $"Created {typeof( T )} : {data.DatabaseIndex}" , newFileContent.ToString() , true )
-				);
+				await OctoKitClient.Repository.Content.CreateFile( SharedSettings.RepositoryUser , SharedSettings.RepositoryName , url , contentRequest );
 			}
 
 		}
