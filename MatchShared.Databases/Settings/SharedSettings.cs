cf8dc9d547369f7ad0ccb75b2705396c63d6db75
Jvs <Jvs34@users.noreply.github.com>
2023-09-20 16:38:11 +0200

    On Second thought, I'm not actually going to use this delegate

--- MatchShared.Databases/Settings/SharedSettings.cs
+++ MatchShared.Databases/Settings/SharedSettings.cs
@@ -13,28 +13,19 @@ namespace MatchTracker
 		public string RoundVideoFile { get; set; }
 		public string RoundVoiceFile { get; set; }
 		public string TimestampFormat { get; set; }
-		public string RoundReplayFile { get; set; }
-		public string RoundReplayFileCompressed { get; set; }
 		public string DatabaseFile { get; set; }
 		public string LevelPreviewFile { get; set; }
 
 		public string DateTimeToString( DateTime time ) => time.ToString( TimestampFormat );
-
-		public static string Combine( Func<string [] , string> combineFunc = null , params string [] paths )
-		{
-			static string DefaultCombineFunction( string [] paths ) => Path.Combine( paths );
-			combineFunc ??= DefaultCombineFunction;
-			return combineFunc( paths );
-		}
-
+		public static string Combine( params string [] paths ) => Path.Combine( paths );
 		public string GetRecordingFolder() => BaseRecordingFolder;
 		public string GetPath<T>( string databaseIndex ) where T : IDatabaseEntry => GetPath( typeof( T ).Name , databaseIndex );
-		public string GetPath( string typeName , string databaseIndex ) => Combine( null , GetRecordingFolder() , typeName , string.IsNullOrEmpty( databaseIndex ) ? typeName : databaseIndex );
-		public string GetDataPath<T>( string databaseIndex = "" ) where T : IDatabaseEntry => Combine( null , GetPath<T>( databaseIndex ) , DataName );
-		public string GetDataPath( string typeName , string databaseIndex = "" ) => Combine( null , GetPath( typeName , databaseIndex ) , DataName );
-		public string GetDatabasePath() => Combine( null , GetRecordingFolder() , DatabaseFile );
-		public string GetRoundVideoPath( string roundName ) => Combine( null , GetPath<RoundData>( roundName ) , RoundVideoFile );
-		public string GetMatchVideoPath( string matchName ) => Combine( null , GetPath<MatchData>( matchName ) , RoundVideoFile );
-		public string GetLevelPreviewPath( string levelName ) => Combine( null , GetPath<LevelData>( levelName ) , LevelPreviewFile );
+		public string GetPath( string typeName , string databaseIndex ) => Combine( GetRecordingFolder() , typeName , string.IsNullOrEmpty( databaseIndex ) ? typeName : databaseIndex );
+		public string GetDataPath<T>( string databaseIndex = "" ) where T : IDatabaseEntry => Combine( GetPath<T>( databaseIndex ) , DataName );
+		public string GetDataPath( string typeName , string databaseIndex = "" ) => Combine( GetPath( typeName , databaseIndex ) , DataName );
+		public string GetDatabasePath() => Combine( GetRecordingFolder() , DatabaseFile );
+		public string GetRoundVideoPath( string roundName ) => Combine( GetPath<RoundData>( roundName ) , RoundVideoFile );
+		public string GetMatchVideoPath( string matchName ) => Combine( GetPath<MatchData>( matchName ) , RoundVideoFile );
+		public string GetLevelPreviewPath( string levelName ) => Combine( GetPath<LevelData>( levelName ) , LevelPreviewFile );
 	}
 }
