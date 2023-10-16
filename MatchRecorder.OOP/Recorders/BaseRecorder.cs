fa23e06d4cdb5c62572c4234e12ca77b9d45a413
Jvs <Jvs34@users.noreply.github.com>
2023-10-16 17:08:35 +0200

    Nah, load the pixel data directly with spans

--- MatchRecorder.OOP/Recorders/BaseRecorder.cs
+++ MatchRecorder.OOP/Recorders/BaseRecorder.cs
@@ -1,15 +1,19 @@
 ﻿using MatchRecorder.Shared.Enums;
 using MatchRecorder.Shared.Messages;
+using MatchRecorder.Shared.Structs;
 using MatchShared.Databases.Interfaces;
 using MatchShared.DataClasses;
 using MatchShared.Enums;
 using Microsoft.Extensions.Logging;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace MatchRecorder.OOP.Recorders;
@@ -248,34 +252,50 @@ internal abstract class BaseRecorder
 
 	internal async Task SaveLevelPreview( LevelPreviewMessage lvlPrev )
 	{
-		string path = Path.Combine( @"C:\Users\Jvsth\Desktop\DeskExample", $"{lvlPrev.LevelName}.png" );
+		string path = GameDatabase.SharedSettings.GetLevelPreviewPath( lvlPrev.LevelName );
+
+		var fileInfo = new FileInfo( path );
+
+		var directoryInfo = fileInfo.Directory;
+
+		if( !directoryInfo.Exists )
+		{
+			directoryInfo.Create();
+		}
+
+		//TODO: use https://docs.sixlabors.com/articles/imagesharp/pixelbuffers.html#parallel-pixel-format-agnostic-image-manipulation
 
 		await Task.Run( () =>
 		{
-			using var test = new Image<Rgba32>( lvlPrev.Width, lvlPrev.Height );
+			//using var test = new Image<Rgba32>( lvlPrev.Width, lvlPrev.Height );
 
-			test.ProcessPixelRows( accessor =>
-			{
-				for( int y = 0;y < accessor.Height;y++ )
-				{
-					var pixelRow = accessor.GetRowSpan( y );
+			var readOnlySpanOfPixels = new ReadOnlySpan<RGBAColor>( lvlPrev.PixelArray );
+			var bytesArray = MemoryMarshal.AsBytes( readOnlySpanOfPixels );
 
-					// pixelRow.Length has the same value as accessor.Width,
-					// but using pixelRow.Length allows the JIT to optimize away bounds checks:
-					for( int x = 0;x < pixelRow.Length;x++ )
-					{
-						// Get a reference to the pixel at position x
-						ref Rgba32 pixel = ref pixelRow[x];
+			using var test = Image.LoadPixelData<Rgba32>( bytesArray, lvlPrev.Width, lvlPrev.Height );
 
-						var col = lvlPrev.PixelArray[( y * accessor.Width ) + x];
+			//test.ProcessPixelRows( accessor =>
+			//{
+			//	for( int y = 0;y < accessor.Height;y++ )
+			//	{
+			//		var pixelRow = accessor.GetRowSpan( y );
 
-						pixel.R = col.R;
-						pixel.G = col.G;
-						pixel.B = col.B;
-						pixel.A = col.A;
-					}
-				}
-			} );
+			//		// pixelRow.Length has the same value as accessor.Width,
+			//		// but using pixelRow.Length allows the JIT to optimize away bounds checks:
+			//		for( int x = 0;x < pixelRow.Length;x++ )
+			//		{
+			//			// Get a reference to the pixel at position x
+			//			ref Rgba32 pixel = ref pixelRow[x];
+
+			//			var col = lvlPrev.PixelArray[( y * accessor.Width ) + x];
+
+			//			pixel.R = col.R;
+			//			pixel.G = col.G;
+			//			pixel.B = col.B;
+			//			pixel.A = col.A;
+			//		}
+			//	}
+			//} );
 
 			test.SaveAsPng( path );
 		} );
